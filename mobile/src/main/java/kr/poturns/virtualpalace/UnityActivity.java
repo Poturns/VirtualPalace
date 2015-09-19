0caadcadf874960579ade24dc533a15da173ff25
MyungJin Kim <weumj1@gmail.com>
2015-09-19 16:06:45 +0900

    Unity 관련 변경

--- mobile/src/main/java/kr/poturns/virtualpalace/UnityActivity.java
+++ mobile/src/main/java/kr/poturns/virtualpalace/UnityActivity.java
@@ -1,6 +1,17 @@
 package kr.poturns.virtualpalace;
 
+import android.view.KeyEvent;
+
 import com.unity3d.player.UnityPlayerActivity;
 
 public class UnityActivity extends UnityPlayerActivity {
+
+    @Override
+    public boolean onKeyDown(int i, KeyEvent keyEvent) {
+        if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            onBackPressed();
+        }
+
+        return super.onKeyDown(i, keyEvent);
+    }
 }
