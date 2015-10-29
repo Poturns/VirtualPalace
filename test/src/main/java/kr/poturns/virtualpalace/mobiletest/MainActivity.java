7f2c0eea6ad3437fee5b8e4a220d0d08c446536b
MyungJin Kim <weumj1@gmail.com>
2015-10-29 20:20:42 +0900

    delete 'ar' package

--- test/src/main/java/kr/poturns/virtualpalace/mobiletest/MainActivity.java
+++ test/src/main/java/kr/poturns/virtualpalace/mobiletest/MainActivity.java
@@ -1,20 +1,12 @@
 package kr.poturns.virtualpalace.mobiletest;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import org.opencv.android.CameraBridgeViewBase;
-
-import kr.poturns.virtualpalace.ArgumentActivity;
-
-public class MainActivity extends ArgumentActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    protected CameraBridgeViewBase getOpenCvCameraView() {
-        return (CameraBridgeViewBase) findViewById(R.id.camera_view);
-    }
 }
