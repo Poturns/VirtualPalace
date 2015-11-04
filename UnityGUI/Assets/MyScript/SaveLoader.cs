a20c56fbb3c14964b9b4df3f52fe9a0080368e01
byeonghun <ebh0128@gmail.com>
2015-11-05 01:18:51 +0900

    FileNotFoundException 예외처

--- UnityGUI/Assets/MyScript/SaveLoader.cs
+++ UnityGUI/Assets/MyScript/SaveLoader.cs
@@ -55,57 +55,80 @@ public  class SaveLoader : MonoBehaviour {
 		BinaryFormatter bf = new BinaryFormatter();
 
 		int ObjectToLoadCnt = ReadBookCaseFile (bf);
+		if (ObjectToLoadCnt <0)
+			return;
 
-		ReadObjFile (bf , ObjectToLoadCnt);
+		ReadObjFile (bf, ObjectToLoadCnt);
 
 	}
 	// 리턴값은 총 오브젝트가 저장된 갯수 
 	private int ReadBookCaseFile(BinaryFormatter bf)
 	{
-		Stream BCStream = File.Open (Application.dataPath + BookCaseFileName , FileMode.Open); 
 		int ObjCnt = 0;
-		int Count = GameObject.Find ("BookCaseGroup").transform.childCount;
-		for (int i = 0; i < Count; i++) 
+		try
 		{
-			SaveDataForBookCase bData = new SaveDataForBookCase();
+			Stream BCStream = File.Open (Application.dataPath + BookCaseFileName , FileMode.Open); 
 
-			bData = (SaveDataForBookCase)bf.Deserialize(BCStream);
-		//	Debug.Log("BookCase" + i + " Name" +" : " +bData.ObjName + "Cnt : " + bData.Cnt);
-			//Book Case Instance -> Gizmo의 Child(0)
-			BookCaseScript BookCase = GameObject.Find (bData.ObjName).transform.GetChild(0).GetComponent<BookCaseScript>();
-			ObjCnt+=bData.Cnt;
-			BookCase.UpdateWithSaveData(bData);
-
+			int Count = GameObject.Find ("BookCaseGroup").transform.childCount;
+			for (int i = 0; i < Count; i++) 
+			{
+				SaveDataForBookCase bData = new SaveDataForBookCase();
+				
+				bData = (SaveDataForBookCase)bf.Deserialize(BCStream);
+				//	Debug.Log("BookCase" + i + " Name" +" : " +bData.ObjName + "Cnt : " + bData.Cnt);
+				//Book Case Instance -> Gizmo의 Child(0)
+				BookCaseScript BookCase = GameObject.Find (bData.ObjName).transform.GetChild(0).GetComponent<BookCaseScript>();
+				ObjCnt+=bData.Cnt;
+				BookCase.UpdateWithSaveData(bData);
+				
+			}
+			BCStream.Close ();
 		}
-		BCStream.Close ();
+		catch(FileNotFoundException e)
+		{
+			ObjCnt = -999;
+		}
+
 		//Debug.Log ("Total Object In BookCase : " + ObjCnt);
 		return ObjCnt;
 	}
-	private void ReadObjFile(BinaryFormatter bf , int ObjCnt)
+	private bool ReadObjFile(BinaryFormatter bf , int ObjCnt)
 	{
-		Stream ObjStream = File.Open (Application.dataPath + ObjFileName , FileMode.Open); 
-		PrefabContainer PrefabCon = GameObject.Find("PreLoadPrefab").GetComponent<PrefabContainer>();
-		for (int i = 0; i < ObjCnt; i++) 
+		try
 		{
-			SaveData sData = new SaveData();
-			sData = (SaveData)bf.Deserialize(ObjStream);
-			GameObject Obj = GameObject.Find (sData.ObjName);
-
-			//처음부터 존재 하는 오브젝트일때 컨텐츠 내용만 업데이트
-			if(Obj != null)
+			Stream ObjStream = File.Open (Application.dataPath + ObjFileName , FileMode.Open); 
+			PrefabContainer PrefabCon = GameObject.Find("PreLoadPrefab").GetComponent<PrefabContainer>();
+			StateManager.GetManager ().ObjCount = ObjCnt -16;
+			for (int i = 0; i < ObjCnt; i++) 
 			{
-				AbstractBasicObject RealObj = Obj.transform.GetChild(0).gameObject.GetComponent<AbstractBasicObject>();
-				RealObj.UpdateContents(sData.Contents);
-			}
-			// 추가되어야할 오브젝트일때 생성후 초기화
-			else
-			{
-				GameObject PrefabToLoad = PrefabCon.GetPrefab(sData.ObjKind);
-				GameObject LoadObject = Instantiate(PrefabToLoad , sData.Pos , sData.Rot) as GameObject;
-				LoadObject.transform.parent = GameObject.Find(sData.ParentName).transform.GetChild(0);
-				LoadObject.transform.GetChild(0).GetComponent<AbstractBasicObject>().UpdateWithSaveData(sData);
+				SaveData sData = new SaveData();
+				sData = (SaveData)bf.Deserialize(ObjStream);
+				GameObject Obj = GameObject.Find (sData.ObjName);
 
+				//처음부터 존재 하는 오브젝트일때 컨텐츠 내용만 업데이트
+				if(Obj != null)
+				{
+					AbstractBasicObject RealObj = Obj.transform.GetChild(0).gameObject.GetComponent<AbstractBasicObject>();
+					RealObj.UpdateContents(sData.Contents);
+				}
+				// 추가되어야할 오브젝트일때 생성후 초기화
+				else
+				{
+					GameObject PrefabToLoad = PrefabCon.GetPrefab(sData.ObjKind);
+					GameObject LoadObject = Instantiate(PrefabToLoad , sData.Pos , sData.Rot) as GameObject;
+					LoadObject.transform.parent = GameObject.Find(sData.ParentName).transform.GetChild(0);
+					LoadObject.transform.GetChild(0).GetComponent<AbstractBasicObject>().UpdateWithSaveData(sData);
+
+				}
 			}
+			ObjStream.Close();
+		
+			return true;
 		}
+		catch(FileNotFoundException e)
+		{
+			return false;
+		}
+
 	}
 }
