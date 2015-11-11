5a635f5a70352ec47040346cd92b23f6dad0ca4b
byeonghun <ebh0128@gmail.com>
2015-11-11 20:47:21 +0900

    AR Object 관리

---
+++ UnityGUI/Assets/ARRenderingObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using BridgeApi.Controller;
+
+
+public class ARRenderingObject : MonoBehaviour {
+	//카메라 근평면 으로 부터의 거리
+	public float zOffset;
+	public ARrenderItem ARItem;
+	void Start () 
+	{
+		zOffset = GameObject.Find ("ARView").transform.position.z / 2;
+	}
+	public void SetARPosition(ARrenderItem item)
+	{
+		//위치를 Screen좌표기준으로 변경
+		transform.position =Camera.main.ScreenToWorldPoint (new Vector3 (item.screenX,
+		                                                                 item.screenY, 
+		                                                                 Camera.main.nearClipPlane+zOffset));
+
+	}
+
+}
--- UnityGUI/Assets/MyScript/States/ARSceneIdleState.cs
+++ UnityGUI/Assets/MyScript/States/ARSceneIdleState.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using MyScript.Interface;
 using BridgeApi.Controller;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace MyScript.States
 {
@@ -8,6 +10,8 @@ namespace MyScript.States
     public class ARSceneIdleState : AbstractGazeInputState, ISceneChangeState
     {
         private GameObject ARScreenObj;
+		private List<ARRenderingObject> ObjList;
+		private GameObject ARObjPrefab;
         public ARSceneIdleState(StateManager managerRef) : base(managerRef, "ARSceneIdleState")
         {
 
@@ -28,6 +32,11 @@ namespace MyScript.States
             // 스크린의 게임오브젝트를 가져온다
             if (ARScreenObj == null)
                 ARScreenObj = GameObject.Find("ARView");
+
+			ObjList = new List<ARRenderingObject> ();
+			ARObjPrefab = GameObject.Find ("PrefabStore").GetComponent<ARPrefabload> ().ARPrefab;
+			if(ARObjPrefab) Debug.Log ("AR Prefab is Null");
+
         }
 
         protected override void HandleCancelOperation()
@@ -48,6 +57,13 @@ namespace MyScript.States
                 case Operation.AR_RENDERING:
                     ARrenderItem item = JsonInterpreter.ParseARrenderItem(operation);
                     Debug.Log("====== AR item : " + item);
+					//Update 리턴값이 false 면 생성
+					if(!ARItemUpdate(item))
+					{
+						GameObject NewObj = GameObject.Instantiate(ARObjPrefab) as GameObject;
+						ARRenderingObject NewAR = ARObjPrefab.GetComponent<ARRenderingObject>();
+						ObjList.Add(NewAR);
+					}
                     break;
 
                 default:
@@ -55,6 +71,26 @@ namespace MyScript.States
             }
 
         }
+		//item을 받아서 List를 검사한뒤 있으면 업데이트하고 ture반환
+		//없으면 false 반환
+		private bool ARItemUpdate(ARrenderItem item)
+		{
+			if (ObjList == null || ObjList.Count == 0) 
+				return false;
+
+			foreach (ARRenderingObject obj in ObjList) 
+			{
+				if(obj.ARItem.resId == item.resId)
+				{
+					obj.ARItem.screenX = item.screenX;
+					obj.ARItem.screenY= item.screenY;
+					obj.SetARPosition(item);
+					return true;
+				}
+			}
+
+			return false;
+		}
     }
 }
 
