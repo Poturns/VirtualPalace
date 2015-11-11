0dfc54eaaad75cafdf821ff1c895641925c251e5
byeonghun <ebh0128@gmail.com>
2015-11-11 23:02:09 +0900

    ARObject 위치 계산 방식 변경 	- Screen / World 변환 에서 비율로 상대좌표계산하는 방식으로
    동작 확인 했으나 ScreenSize가 제대로 동작하는지 확인 필

--- UnityGUI/Assets/ARRenderingObject.cs
+++ UnityGUI/Assets/ARRenderingObject.cs
@@ -7,9 +7,11 @@ public class ARRenderingObject : MonoBehaviour {
 	//카메라 근평면 으로 부터의 거리
 	public float zOffset;
 	public ARrenderItem ARItem;
+	public GameObject arView;
 	void Start () 
 	{
-		zOffset = GameObject.Find ("ARView").transform.position.z;
+		arView = GameObject.Find ("ARView");
+		zOffset = arView.transform.position.z;
 	}
 	public void SetARItem(ARrenderItem item)
 	{
@@ -17,12 +19,30 @@ public class ARRenderingObject : MonoBehaviour {
 		ARItem.screenY= item.screenY;
 		ARItem.resId = item.resId;
 	}
+	//비율을 구한다음 PosDummy(parent)로부터의 상대 위치를 구해서 위치 변경
 	public void SetARPosition(ARrenderItem item)
 	{
+		if (arView == null) 
+		{
+			arView = GameObject.Find("ARView");
+		}
+		float Width = Screen.width;
+		float Height = Screen.height;
+
+		float XRatio = item.screenX / Width;
+		float YRatio = item.screenY / Height;
+		if (XRatio > 1)
+			XRatio = 1;
+		if (YRatio > 1)
+			YRatio = 1;
+
+		float RelativeX = XRatio * arView.transform.lossyScale.x;
+		float RelativeY = YRatio * arView.transform.lossyScale.y;
+		Transform Parent = transform.parent;
 		//위치를 Screen좌표기준으로 변경
-		transform.position =Camera.main.ScreenToWorldPoint (new Vector3 (item.screenX,
-		                                                                 item.screenY, 
-		                                                                 Camera.main.nearClipPlane+zOffset));
+		transform.position =new Vector3 (Parent.position.x + RelativeX,
+                                         Parent.position.y + RelativeY, 
+                                         Parent.position.z);
 
 	}
 
