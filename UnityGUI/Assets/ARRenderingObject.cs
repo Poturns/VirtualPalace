aac3e067aa2bf4d439cf6dc2689e2a4edea5572b
byeonghun <ebh0128@gmail.com>
2015-11-12 18:50:20 +0900

    깊이문제 해결

--- UnityGUI/Assets/ARRenderingObject.cs
+++ UnityGUI/Assets/ARRenderingObject.cs
@@ -9,10 +9,13 @@ public class ARRenderingObject : AbstractBasicObject {
 	//카메라 근평면 으로 부터의 거리
 	public float zOffset;
 	public ARrenderItem ARItem;
+	public GameObject ARHead;
+
 	public GameObject arView;
 	void Start () 
 	{
 		arView = GameObject.Find ("ARView");
+		ARHead = GameObject.Find ("Head");
 		zOffset = arView.transform.position.z;
 	}
 	public void SetARItem(ARrenderItem item)
@@ -33,6 +36,10 @@ public class ARRenderingObject : AbstractBasicObject {
 		{
 			arView = GameObject.Find("ARView");
 		}
+		if (ARHead == null) 
+		{
+			ARHead = GameObject.Find ("Head");
+		}
 		float Width = Screen.width;
 		float Height = Screen.height;
 
@@ -45,12 +52,25 @@ public class ARRenderingObject : AbstractBasicObject {
 
 		float RelativeX = XRatio * arView.transform.lossyScale.x;
 		float RelativeY = YRatio * arView.transform.lossyScale.y;
+
+	
+		Vector3 HeadVector = ARHead.transform.forward;
+		Vector3 MoveHeadVector = ARHead.transform.position + 0.01f*HeadVector;
+
+		//HeadVector.Normalize ();
 		Transform Parent = transform.parent;
+		Debug.Log (" z :::::::::::" + Parent.position.z);
 		//위치를 Screen좌표기준으로 변경
-		transform.position =new Vector3 (Parent.position.x + RelativeX,
-                                         Parent.position.y + RelativeY, 
-                                         Parent.position.z);
+		transform.position =new Vector3 (Parent.position.x,
+                                         Parent.position.y, 
+		                                 Parent.position.z);
+		transform.localPosition = new Vector3 (RelativeX*transform.lossyScale.x, RelativeY*transform.lossyScale.y, -0.01f);
 
 	}
 
+	void Update()
+	{
+		if(arView != null)
+			gameObject.transform.rotation= arView.transform.rotation;
+	}
 }
